d8081f943577eb620527232627bcb30def68a4d5
AlvesCosta <alvescosta74@gmail.com>
2025-10-03 20:21:06 -0300

    first commit

---
+++ test_pgsql.php
@@ -0,0 +1,16 @@
+<?php
+if (extension_loaded('pdo_pgsql')) {
+    echo "PDO PostgreSQL está instalado\n";
+} else {
+    echo "PDO PostgreSQL NÃO está instalado\n";
+}
+
+if (extension_loaded('pgsql')) {
+    echo "PostgreSQL está instalado\n";
+} else {
+    echo "PostgreSQL NÃO está instalado\n";
+}
+
+echo "Extensões carregadas:\n";
+print_r(get_loaded_extensions());
+?>
